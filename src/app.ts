6f9f35c4616316d695cfa5a2cb8e13a2a3cc51a9
Antony Harfield <antonyharfield@gmail.com>
2021-02-24 17:13:55 +0700

    Add support for accept snake

--- src/app.ts
+++ src/app.ts
@@ -7,18 +7,25 @@ import streams from './streams/router'
 import projects from './projects/router'
 import { jwtCheck } from './common/auth'
 import { errorHandler, notFoundHandler } from './common/error-handlers'
+import responseJsonHandler from './common/serializers/response-json'
 
+// Configuration
 const app = express()
+app.response.json = responseJsonHandler
+
+// Middleware
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(jwtCheck)
 
+// Routes
 app.use('/assets', assets)
 app.use('/deployments', deployments)
 app.use('/streams', streams)
 app.use('/projects', projects)
 
+// Fallbacks
 app.use(errorHandler)
 app.use(notFoundHandler)
 
---
+++ src/common/serializers/response-json.ts
@@ -0,0 +1,11 @@
+import type { Response } from 'express'
+import { camelToSnake } from './snake-camel'
+
+export default function (this: Response, objectOrArray: any): void {
+  const accepts = this.req?.header('Accept')
+  if (accepts === 'application/json;format=snake') {
+    objectOrArray = camelToSnake(objectOrArray, 8)
+  }
+  this.set('Content-Type', 'application/json')
+  this.status(200).send(JSON.stringify(objectOrArray))
+}
