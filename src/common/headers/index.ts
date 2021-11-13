14bae589d193adf2b9dd78d348e71a8f5c9866fd
wasuphon <wasuphonn59@email.nu.ac.th>
2021-11-14 00:14:56 +0700

    to arrow function

--- src/common/headers/index.ts
+++ src/common/headers/index.ts
@@ -1,6 +1,6 @@
 import { AppInfo } from 'src/types'
 
-export const userAgentToAppInfo = function (userAgent: string | undefined): AppInfo | undefined {
+export const userAgentToAppInfo = (userAgent: string | undefined): AppInfo | undefined => {
   if (userAgent === undefined) return undefined
   const appInfo = userAgent.split('/')
   return { name: appInfo[0], versionName: appInfo[1], versionCode: +appInfo[2] }
