3e4a5296d3c7af7393cd113fc3b0b70b8a796c58
TooSeriuz <frongs@rfcx.org>
2023-03-09 18:52:25 +0700

    rename assetParams to assetParameters

--- src/deployments/router.ts
+++ src/deployments/router.ts
@@ -96,7 +96,7 @@ router.get('/:id/assets', (req: Request, res: Response, next: NextFunction): voi
 router.post('/:id/assets', multerFile.single('file'), (req: Request, res: Response, next: NextFunction): void => {
   const deploymentId = req.params.id
   const file = req.file ?? null
-  const parameter = req.body.assetParams
+  const parameter = req.body.assetParameters
 
   dao.getStreamIdById(deploymentId).then(async streamId => {
     const assetId = await service.uploadFileAndSaveToDb(streamId, deploymentId, file, parameter)
