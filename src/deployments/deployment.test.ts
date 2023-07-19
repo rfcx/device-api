7f0a120a5a52b9c8adbf2076a131fe494f5007f0
TooSeriuz <frongs@rfcx.org>
2023-07-19 16:32:13 +0700

    Fix tests by comment

--- src/deployments/deployment.test.ts
+++ src/deployments/deployment.test.ts
@@ -297,7 +297,7 @@ describe('GET /deployments', () => {
     await Deployment.create(mockDeployment1)
     await Deployment.create(mockDeployment2)
 
-    const response = await request(app).get('?type=audiomoth')
+    const response = await request(app).get(`/`).query({ type: 'audiomoth'})
 
     expect(response.statusCode).toBe(200)
     expect(response.body).toHaveLength(1)
@@ -313,7 +313,7 @@ describe('GET /deployments', () => {
     await Deployment.create(mockDeployment1)
     await Deployment.create(mockDeployment2)
 
-    const response = await request(app).get('?type=guardian')
+    const response = await request(app).get(`/`).query({ type: 'guardian'})
 
     expect(response.statusCode).toBe(200)
     expect(response.body).toHaveLength(2)
@@ -329,15 +329,15 @@ describe('GET /deployments', () => {
     await Deployment.create(mockDeployment1)
     await Deployment.create(mockDeployment2)
 
-    const response = await request(app).get('?type=guardian')
+    const response = await request(app).get(`/`).query({ type: 'guardian'})
 
     expect(response.statusCode).toBe(200)
     expect(response.body).toHaveLength(0)
   })
 
   test('get 2 deployment by project id', async () => {
-    const deploymentId1 = '0000000000000000'
-    const deploymentId2 = '0000000000000001'
+    const deploymentId1 = '00000000000000A0'
+    const deploymentId2 = '00000000000000B0'
     const streamId1 = 'aaaaaaaaaaa1'
     const streamId2 = 'aaaaaaaaaaa2'
     const mockDeployment1 = { id: deploymentId1, streamId: streamId1, deploymentType: 'audiomoth', deployedAt: '2021-05-12T05:21:21.960Z', isActive: true, createdById: seedValues.primarySub }
@@ -347,17 +347,21 @@ describe('GET /deployments', () => {
     const mockStreamReturn = [{ id: streamId1}, { id: streamId2}] as StreamResponse[]
 
     const spy = jest.spyOn(api, 'getStreams').mockReturnValue(Promise.resolve(mockStreamReturn))
-    const response = await request(app).get(`?projectIds=000000000000`)
+    const response = await request(app).get(`/`).query({ projectIds: ['000000000000']})
 
     expect(spy).toHaveBeenCalled()
 
     expect(response.statusCode).toBe(200)
     expect(response.body).toHaveLength(2)
+    expect(response.body[0].id).toBe(deploymentId1)
+    expect(response.body[0].streamId).toBe(streamId1)
+    expect(response.body[1].id).toBe(deploymentId2)
+    expect(response.body[1].streamId).toBe(streamId2)
   })
 
   test('get 2 deployment by project ids', async () => {
-    const deploymentId1 = '0000000000000000'
-    const deploymentId2 = '0000000000000001'
+    const deploymentId1 = '00000000000000A1'
+    const deploymentId2 = '00000000000000B1'
     const streamId1 = 'aaaaaaaaaaa1'
     const streamId2 = 'aaaaaaaaaaa2'
     const mockDeployment1 = { id: deploymentId1, streamId: streamId1, deploymentType: 'audiomoth', deployedAt: '2021-05-12T05:21:21.960Z', isActive: true, createdById: seedValues.primarySub }
@@ -367,12 +371,16 @@ describe('GET /deployments', () => {
     const mockStreamReturn = [{ id: streamId1}, { id: streamId2}] as StreamResponse[]
 
     const spy = jest.spyOn(api, 'getStreams').mockReturnValue(Promise.resolve(mockStreamReturn))
-    const response = await request(app).get(`?projectIds=000000000000&projectIds=000000000001`)
+    const response = await request(app).get(`/`).query({ projectIds: ['000000000000', '000000000001']})
 
     expect(spy).toHaveBeenCalled()
 
     expect(response.statusCode).toBe(200)
     expect(response.body).toHaveLength(2)
+    expect(response.body[0].id).toBe(deploymentId1)
+    expect(response.body[0].streamId).toBe(streamId1)
+    expect(response.body[1].id).toBe(deploymentId2)
+    expect(response.body[1].streamId).toBe(streamId2)
   })
 
   test('fail when get deployment with streamIds and projectIds', async () => {
@@ -385,7 +393,7 @@ describe('GET /deployments', () => {
     await Deployment.create(mockDeployment1)
     await Deployment.create(mockDeployment2)
 
-    const response = await request(app).get(`?streamIds=000000000000&projectIds=000000000001`)
+    const response = await request(app).get(`/`).query({ streamIds: ['000000000000'], projectIds: ['000000000001']})
 
     expect(response.statusCode).toBe(400)
   })
