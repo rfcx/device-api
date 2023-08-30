f72efc14b569d22c1faf66bc5c2d976fb366e926
TooSeriuz <frongs@rfcx.org>
2023-08-30 16:47:23 +0700

    update existing elephant model

--- src/classifiers/router.ts
+++ src/classifiers/router.ts
@@ -23,16 +23,16 @@ router.get('/', (req: Request, res: Response, next: NextFunction): void => {
     {
       id: '1637901623151',
       name: 'asia-elephant-edge',
-      version: '2',
-      path: 'https://rfcx-install.s3.eu-west-1.amazonaws.com/rfcx-guardian/guardian-asset-classifier/1637901623151.tflite.gz',
+      version: '3',
+      path: 'https://rfcx-install.s3.eu-west-1.amazonaws.com/rfcx-guardian/guardian-asset-classifier/1692966951000.tflite.gz',
       type: 'tflite',
-      sha1: '69482d8b65083e2fabcf1096033c863409cc50f7',
+      sha1: 'f809836421d42fa529032b8fb4567f7995801ed9',
       sample_rate: '8000',
       input_gain: '1.0',
       window_size: '2.5000',
       step_size: '2.0000',
-      classifications: 'elephas_maximus,environment',
-      classifications_filter_threshold: '0.98,1.00'
+      classifications: 'elephas_maximus_courtship_song,elephas_maximus_simple_call,elephas_maximus_simple_call_2,elephas_maximus_territorial_song',
+      classifications_filter_threshold: '0.98,0.98,0.98,0.98'
     },
     {
       id: '1692966951000',
